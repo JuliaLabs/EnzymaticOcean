b17997a1f6570a90c47828c188c2bdddebd0006b
Gregory Wagner <wagner.greg@gmail.com>
2021-05-03 12:58:36 -0700

    Adds convective adjustment example

--- convective_adjustment/convective_adjustment.jl
+++ convective_adjustment/convective_adjustment.jl
@@ -1,27 +1,81 @@
+using UnicodePlots
+
 include("convective_adjustment_utils.jl")
 
 # Problem parameters
 grid = RegularGrid(Nz=32, Lz=128)
 
-dTdz = 1e-4
-κᶜ = 1.0
-κᵇ = 1e-2
-surface_flux = 1e-6
-dt = 0.01
-Nt = 10
+convection_diffusivity = 10.0  # [m² s⁻¹]
+background_diffusivity = 1e-4 # [m² s⁻¹]
+surface_flux = 1e-4 # [m s⁻¹ ᶜC]
+@show time_step = 0.2 * grid.Δz^2 / convection_diffusivity # s
+stop_time = 1800 # seconds
+@show Nt = ceil(Int, stop_time / time_step)
 
 # Problem data
-T_out = CUDA.zeros(grid.Nz, Nt)
-T_truth = CUDA.zeros(grid.Nz, Nt)
-scratch = CUDA.zeros(grid.Nz, Nt)
-error = Float64[]
+temperature = zeros(grid.Nz, Nt)
 
-T = 20 .+ dTdz * CuArray(-grid.Nz+1/2:-1/2) .* grid.Δz
-T .= 20 + grid.
+# Set initial condition
+z = zᶜ(grid)
+temperature_gradient = 1e-4
+temperature[:, 1] .= 20 .+ temperature_gradient .* z
+lineplot(z, temperature[:, 1])
 
-let kernel = uncertain_convect!(CUDADevice())
-    ev = kernel(error, κᵇ, κᶜ, surface_flux, scratch, T_truth, T_out, T, grid, dt, Nt, ndrange=size(T))
-    wait(ev)
-    
-    @show all(Array(out) .== 1.0)
+let kernel = convect!(CPU())
+    event = kernel(temperature,
+                   grid,
+                   background_diffusivity,
+                   convection_diffusivity,
+                   surface_flux,
+                   time_step,
+                   Nt,
+                   ndrange = size(temperature, 1))
+
+    wait(event)
+
+end
+
+pl = lineplot(temperature[:, 1], z)
+lineplot!(pl, temperature[:, end], z)
+
+# Gradient
+mean_error = [0.0]
+pointwise_error = zeros(grid.Nz, Nt)
+temperature_data = deepcopy(temperature)
+
+# Parameter estimation: estimate free parameters by
+# calibrading to "truth" or "data":
+
+# Perturb free parameters
+perturbed_background_diffusivity = background_diffusivity * 1.1
+perturbed_convection_diffusivity = convection_diffusivity * 0.8
+
+#=
+# State estimation: produce an estimate temperature *state* (in space and time)
+# by optimizing initial condition and boundary conditions.
+
+# Corrupt initial condition
+temperature[:, 1] .+= temperature_gradient * grid.Lz * 1e-1 * rand()
+
+# Corrupt boundary condition
+surface_flux = surface_flux * 1.1
+=#
+
+let kernel = uncertain_convect!(CPU())
+
+    event = kernel(mean_error,
+                   pointwise_error,
+                   grid,
+                   perturbed_background_diffusivity,
+                   perturbed_convection_diffusivity,
+                   surface_flux,
+                   time_step,
+                   Nt,
+                   temperature_data,
+                   temperature,
+                   ndrange = size(temperature, 1))
+
+    wait(event)
+
+    @show mean_error
 end
--- convective_adjustment/convective_adjustment_utils.jl
+++ convective_adjustment/convective_adjustment_utils.jl
@@ -1,5 +1,7 @@
 using KernelAbstractions
+using KernelAbstractions.Extras: @unroll
 using CUDAKernels
+using Statistics
 
 # Grid
 struct RegularGrid
@@ -10,61 +12,66 @@ end
 
 RegularGrid(; Nz, Lz) = RegularGrid(Nz, Lz, Lz / Nz)   
 
+zᶜ(grid::RegularGrid) = (-grid.Nz+1/2:-1/2) * grid.Δz
+
 # Derivatives
-∂zᶠ(i, grid, T) = (T[i] - T[i-1]) / grid.Δz
-∂zᶜ(i, grid, F) = (F[i+1] - F[i]) / grid.Δz
-∂zᶜ(i, grid, F::Function, args...) = (F(i+1, grid, args...) - F(i, grid, args...)) / grid.Δz
+@inline ∂zᶠ(i, grid, T::AbstractArray) = @inbounds (T[i] - T[i-1]) / grid.Δz
+@inline ∂zᶜ(i, grid, F::AbstractArray) = @inbounds (F[i+1] - F[i]) / grid.Δz
+
+@inline ∂zᶠ(i, grid, f::F, args...) where F<:Function = (f(i, grid, args...) - f(i-1, grid, args...)) / grid.Δz
+@inline ∂zᶜ(i, grid, f::F, args...) where F<:Function = (f(i+1, grid, args...) - f(i, grid, args...)) / grid.Δz
 
 diffusivity(i, grid, κᵇ, κᶜ, T) = ifelse(∂zᶠ(i, grid, T) < 0, κᶜ, κᵇ)
 
-diffusive_flux(i, grid, κᵇ, κᶜ, surface_flux, T) = ifelse(i == 1, 0,
-                                                   ifelse(i == grid.Nz+1, surface_flux, diffusivity(i, grid, κᵇ, κᶜ, T) * ∂zᶠ(i, grid, T))
+function diffusive_flux(i, grid, κᵇ, κᶜ, surface_flux, T)
+    if i < 2 # bottom at i = 1
+        return 0
+    elseif i < grid.Nz + 1 # interior
+        return - diffusivity(i, grid, κᵇ, κᶜ, T) * ∂zᶠ(i, grid, T)
+    else # top boundary
+        return surface_flux
+    end
+end
 
-"""Conduct forward run with no error / loss calculation."""
-@kernel function convect!(T_out, κᵇ, κᶜ, surface_flux, T, grid, dt, Nt)
-
+"""
+Conduct forward run with no error / loss calculation.
+The initial state must be stored in T[:, 1]. """
+@kernel function convect!(T, grid, κᵇ, κᶜ, surface_flux, Δt, Nt)
     i = @index(Global)
 
-    @unroll for n = 1:Nt
-        T_out[i, n] = dt * ∂zᶜ(i, grid, diffusive_flux, κᵇ, κᶜ, surface_flux, T)
+    Tⁿ⁻¹(n) = view(T, :, n-1)
 
+    @unroll for n = 2:Nt
+        T[i, n] = T[i, n-1] - Δt * ∂zᶜ(i, grid, diffusive_flux, κᵇ, κᶜ, surface_flux, view(T, :, n-1))
         @synchronize
-
-        T[i] = T_out[i, n]
     end
 end
 
 """Conduct forward run and assess error against T_truth."""
-@kernel function uncertain_convect!(error,                  # Output (a 0D array containing a scalar)
-                                    κᵇ,  # Parameter (we want to differentiate with respect to this)
-                                    κᶜ, # Parameter (we want to differentiate with respect to this)
-                                    surface_flux,           # Surface boundary condition
-                                    scratch,                # scratch space
-                                    @Const(T_truth),        # "Truth" (used to evaluate model fidelity)
-                                    T_out,                  # Stores the model output
-                                    T,                      # Stores the "current" state
-                                    grid,                   # Represents the physical domain
-                                    dt,                     # Time increment
-                                    Nt)                     # Number of time-steps
+@kernel function uncertain_convect!(mean_error,      # Output (a 0D array containing a scalar)
+                                    pointwise_error, # Output (a 0D array containing a scalar)
+                                    grid,            # Represents the physical domain
+                                    κᵇ,              # Free parameter: "background" diffusivity
+                                    κᶜ,              # Free parameter: diffusiity in convective plumes
+                                    surface_flux,    # Fixed parameter: surface boundary condition
+                                    Δt,              # Fixed parameter: time increment
+                                    Nt,              # Fixed parameter: of time-steps
+                                    T_truth,         # Data: Nz by Nt matrix of vertical-temporal temperature profile
+                                    T)               # Scratch vector that stores the "current" temperature profile
     i = @index(Global)
 
-    @unroll for n = 1:Nt
-        T_out[i, n] = dt * ∂zᶜ(i, grid, diffusive_flux, κᵇ, κᶜ, T)
-
+    @unroll for n = 2:Nt
+        T[i, n] = T[i, n-1] - Δt * ∂zᶜ(i, grid, diffusive_flux, κᵇ, κᶜ, surface_flux, view(T, :, n-1))
         @synchronize
-
-        T[i] = T_out[i, n]
     end
 
-    @synchronize
-
     @unroll for n = 1:Nt
-        scratch[i, n] = (T_out[i, n] - T_truth[i, n])^2
+        pointwise_error[i, n] = (T[i, n] - T_truth[i, n])^2
     end
 
     @synchronize
 
     if i == 1
-        error[] = mean(error_scratch)
+        mean_error[1] = mean(pointwise_error)
     end
 end
