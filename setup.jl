9f97a8e78c1eb2f1a77513ea690d3aabffccea9c
Valentin Churavy <v.churavy@gmail.com>
2021-05-17 20:58:34 -0400

    Zygote + Enzyme + convective_adjustment

--- setup.jl
+++ setup.jl
@@ -4,10 +4,16 @@ using Enzyme
 
 # TODO: The need for this currently prevents us from turning this into a neat package
 #       Can we do this without requiring the specific kernel?
-@inline function CUDAKernels.Cassette.overdub(::CUDAKernels.CUDACtx, ::typeof(Enzyme.autodiff), f, args...)
+@inline function CUDAKernels.Cassette.overdub(::CUDAKernels.CUDACtx, ::typeof(Enzyme.autodiff_no_cassette), f, args...)
     f′ = (args...) -> (Base.@_inline_meta; CUDAKernels.Cassette.overdub(CUDAKernels.CUDACTX, f, args...))
-    Enzyme.autodiff(f′, args...)
+    Enzyme.autodiff_no_cassette(f′, args...)
 end
+
+@inline function KernelAbstractions.Cassette.overdub(::KernelAbstractions.CPUCtx, ::typeof(Enzyme.autodiff_no_cassette), f, args...)
+    f′ = (args...) -> (Base.@_inline_meta; KernelAbstractions.Cassette.overdub(KernelAbstractions.CPUCTX, f, args...))
+    Enzyme.autodiff_no_cassette(f′, args...)
+end
+
 
 function Enzyme.autodiff(kernel::KernelAbstractions.Kernel)
     function df(ctx, args...)
