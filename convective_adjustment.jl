f1588d94cbe01dbf1fec985072fbf8e264a41514
Valentin Churavy <v.churavy@gmail.com>
2021-05-21 20:14:51 -0400

    Fluxify

--- convective_adjustment.jl
+++ convective_adjustment.jl
@@ -1,5 +1,5 @@
 include("convective_adjustment_utils.jl")
-using Zygote
+using Flux
 
 # Problem parameters
 grid = RegularGrid(Nz=32, Lz=128)
@@ -13,59 +13,57 @@ stop_time = 1800 # seconds
 
 # Problem data
 if USE_CPU 
-    temperature = zeros(Float64, grid.Nz, Nt)
+    T0 = zeros(Float64, grid.Nz)
 else
-    temperature = CUDA.zeros(Float64, grid.Nz, Nt)
+    T0 = CUDA.zeros(Float64, grid.Nz)
 end
 
 # Set initial condition
 z = zᶜ(grid)
 temperature_gradient = 1e-4
-temperature[:, 1] .= 20 .+ temperature_gradient .* z
+T0 .= 20 .+ temperature_gradient .* z
 
-function dothething(T, grid, κᵇ, κᶜ, surface_flux, Δt, Nt)
-    kb = adapt(typeof(T), [κᵇ])
-    kc = adapt(typeof(T), [κᶜ])
-    prev=nothing
-    for t in 2:Nt
-        prev = convect!(T, grid, kb, kc, surface_flux, Δt, t; prev)
+struct Model{AT}
+    κᵇ::AT
+    κᶜ::AT
+end
+Model(::Type{T}, κᵇ, κᶜ) where T = Model(adapt(T, [κᵇ]), adapt(T, [κᶜ]))
+Flux.@functor Model
+
+function (m::Model)(T, grid, surface_flux, Δt, Nt)
+    for _ in 2:Nt
+        T = convect!(T, grid, m.κᵇ, m.κᶜ, surface_flux, Δt)
     end
-    wait(prev)
+    T
 end
 
-function reversethething(T, dT, grid, κᵇ, κᶜ, surface_flux, Δt, Nt)
-    kb  = adapt(typeof(T), [κᵇ])
-    dkb = similar(kb)
-    dkb .= 0
+model = Model(typeof(T0), background_diffusivity, convection_diffusivity) 
 
-    kc  = adapt(typeof(T), [κᶜ])
-    dkc = similar(kc)
-    dkc .= 0
-    
-    prev=nothing
-    for t in Nt:-1:2
-       prev = gradconvect!(T, dT, grid, kb, dkb, kc, dkc, surface_flux, Δt, t; prev)
+# Perfect model
+Tdata = model(T0, grid, surface_flux, time_step, Nt)
+@show T0
+@show Tdata
+
+
+function fit(model, Tdata; N=10)
+    opt = ADAM()
+    ps = params(model)
+    for _ in 1:N
+        # Calculate error using Zygote
+        gs = gradient(ps) do 
+            T = model(T0, grid, surface_flux, time_step, Nt)
+            mean((T .- Tdata).^2)
+        end
+        Flux.Optimise.update!(opt, ps, gs)
     end
-    wait(prev)
-    dkb[1], dkc[1]
+    return model.κᵇ[1], model.κᶜ[1]
 end
 
-dothething(temperature, grid, background_diffusivity, convection_diffusivity, surface_flux, time_step, Nt)
+model = Model(typeof(T0), background_diffusivity * 1.2, convection_diffusivity * 0.7)
+@show fit(model, Tdata; N = 100)
 
-@show temperature[3, Nt]
-
-# Gradient
-temperature_data = temperature .+ adapt(typeof(temperature), randn(size(temperature)...))
-
-# Calculate error using Zygote
-dt, = gradient(temperature) do temperature
-    mean((temperature .- temperature_data).^2)
-end
-
-# Enzyme.API.printall!(true)
-# Enzyme.API.printtype!(true)
-
-dkb, dkc = reversethething(temperature, dt, grid, background_diffusivity, convection_diffusivity, surface_flux, time_step, Nt)
-
-@show dkb
-@show dkc
+# κᵇ, κᶜ = background_diffusivity * 100, convection_diffusivity * 0.7
+# o1 = mean( (model(T0, grid, κᵇ, κᶜ, surface_flux, time_step, Nt) .- Tdata).^2 )
+# κᵇ += 1e-6
+# o2 = mean( (model(T0, grid, κᵇ, κᶜ, surface_flux, time_step, Nt) .- Tdata).^2 )
+# @show (o2 - o1) / 1e-6
