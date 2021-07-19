ea07dda6c598d201a8de6b3fa3bb6ab4cbefa5ae
Ludger Paehler <ludger.paehler@tum.de>
2021-07-19 15:08:24 +0200

    Create data to model against.

--- gen_test.jl
+++ gen_test.jl
@@ -16,17 +16,13 @@ surface_flux = 1e-4
 T0 .= 20 .+ temperature_gradient .* z
 
 
-# Convective adjustment function
-@gen function convective_adjustment(grid, surface_flux, T)
-    # Construct priors (debug priors for now)
-    convective_diffusivity = @trace(normal(10, 2), :convective_diffusivity)
-    background_diffusivity = @trace(normal(1e-4, 3e-5), :background_diffusivity)
-
-    # Calculate Δt & Nt -> Keeps adjusting all the time
+# Draw up a perfect model
+function model(grid, surface_flux, T, convective_diffusivity, background_diffusivity)
+    # Calculate Δt & Nt
     Δt = 0.2 * grid.Δz^2 / convective_diffusivity
     Nt = ceil(Int, stop_time / Δt)
 
-    # Wrap priors into arrays for Enzyme
+    # Wrap into arrays for Enzyme
     convective_diffusivity = adapt(typeof(T), [convective_diffusivity])
     background_diffusivity = adapt(typeof(T), [background_diffusivity])
 
@@ -34,14 +30,41 @@ T0 .= 20 .+ temperature_gradient .* z
         T = convect!(T, grid, background_diffusivity, convective_diffusivity, surface_flux, Δt)
     end
     return T
-
 end
 
-convective_adjustment(grid, surface_flux, T0)
+
+# Convective adjustment function
+@gen function convective_adjustment(grid, surface_flux, T)
+    # Construct priors (debug priors for now)
+    convective_diffusivity = @trace(normal(10, 2), :convective_diffusivity)
+    background_diffusivity = @trace(normal(1e-4, 3e-5), :background_diffusivity)
+
+    model(grid, surface_flux, T, convective_diffusivity, background_diffusivity)  # Do I need to genify this at this point?
+end
 
 
+# Generate the perfect data -> QMC/LHS across the space of temperature gradient
+t_data = []
+for i in 1:Nt
+    # Fixed real value
+    convective_diffusivity = 10
+    background_diffusivity = 1e-4
 
-#function infer()
+    # LHS of temperature_gradient
+    T_start = 20 .+ temporary_temp_gradient .* z
+    t_data[i] = model(grid, surface_flux, T_start, convective_diffusivity, background_diffusivity)
+end
 
 
-#end
+function importance_sampling_inference(model, grid, surface_flux, T, ys, amount_of_computation)
+
+    observations = Gen.choicemap()
+    for (i, y) in enumerate(ys)
+        observations[(:y, i)] = y
+    end
+
+    # Perform importance sampling to find the most likely simulation trace
+    # consistent with our observations
+    (trace, lml_est) = Gen.importance_sampling(model, (grid, surface_flux, T), observations, amount_of_computation)
+    return trace, lml_est
+end
